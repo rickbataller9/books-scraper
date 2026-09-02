b2ed95882bf1de364ae8f06b8990959b16c24611
rbataller <martinbataller556@gmail.com>
2026-09-02 15:50:42 +0800

    Clean and type fields in parse_page

--- scrape.py
+++ scrape.py
@@ -3,11 +3,13 @@ import time
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 CACHE = Path("cache")
 CACHE.mkdir(exist_ok=True)
 
 HEADERS = {"User-Agent": "Mozilla/5.0 (learning project)"}
+RATINGS = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
 
 
 def get(url, delay=1.0):
@@ -25,22 +27,20 @@ def get(url, delay=1.0):
     time.sleep(delay)
     return r.text
 
-#.product_pod
-#   - h3 a
-#   - .product_price .price_color
-#   - .star-rating
-
-def parse_page(html):
-    soup = BeautifulSoup(html, "lxml")
+def parse_page(html, base_url):
+    pods = BeautifulSoup(html, "lxml")
     books = []
 
-    for pod in soup.select(".product_pod"):
+    for pod in pods.select(".product_pod")[:3]:
         books.append({
             "title": pod.select_one("h3 a")["title"],
-            "price": pod.select_one(".price_color").text,
-            "rating": pod.select_one(".star-rating")["class"],
-            "stock": pod.select_one(".instock.availability").text,
-            "url": pod.select_one("h3 a")["href"],
+            "rating": RATINGS.get(pod.select_one(".star-rating")["class"][-1]),
+            "price": float(pod.select_one(".price_color").text.replace("£", "").replace(",", "")),
+            "stock": 'In stock' in pod.select_one(".instock.availability").text.strip(),
+            "url": urljoin(base_url, pod.select_one("h3 a")["href"])
         })
 
     return books
+
+books = parse_page(get("https://books.toscrape.com/"), https://books.toscrape.com/)
+print(books[0])
